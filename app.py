657108828711e4f3d7db1b549bc6102abf83deb1
JUan Cruz <juanchoalric94@gmail.com>
2021-10-11 15:33:06 -0300

    saque endpoint dummy

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://dbuser:Mvkvemu7tfb691sS@cluster0.hobus.mongodb.net/seminario?retryWrites=true&w=majority"
+db = PyMongo(app).db
+
+@app.route("/register", methods=["POST"])
+def register_user():
+    db.user.insert_one({
+        "_id": request.form["document"],
+        "name": request.form["name"],
+        "surname": request.form["surname"],
+        "password": request.form["password"],
+    })
+
+    return "Hello world"
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    user = db.user.find_one({'_id': request.form["document"], "password": request.form["password"]})
+    return jsonify(user)
+
+if __name__ == "__main__":
+    app.run(port=4032)
