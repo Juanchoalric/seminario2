4a256f58c6dd41bb298aa6de8168c1526df80ff2
SECUREAUTH\enenadovit <emmanuelnenadovit@gmail.com>
2021-10-14 21:03:44 -0300

    - Added post and get for entries

--- README.md
+++ README.md
@@ -20,6 +20,12 @@
  "password": "loquesea"
 }
 
+{
+ "name": "GeneralStore",
+ "email": "generalstore@gmail.com",
+ "password": "loquesea"
+}
+
 ### /login
 
 #### usuario
@@ -32,4 +38,31 @@
 {
  "email": "pepe2@gmail.com",
  "password": "loquesea"
+}
+
+{
+ "email": "generalstore@gmail.com",
+ "password": "loquesea"
+}
+
+### /entry (POST)
+
+{
+ "user_name": jose,
+ "store_name": "GeneralStore"
+}
+
+### /entry (GET)
+
+example (local url) with postman: http://127.0.0.1:4032/entry?store_name=GeneralStore
+example (heroku url) with postman: http://viralert2.herokuapp.com/entry?store_name=GeneralStore
+
+response: 
+
+{
+    "_id": "028171f5896c4aca9c067e363b186870",
+    "date": "14-Oct-2021",
+    "store_name": "GeneralStore",
+    "time": "19:52:07",
+    "user_name": "jose"
 }
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from datetime import datetime
 import uuid
 
 app = Flask(__name__)
@@ -46,6 +47,29 @@ def login_user():
         return jsonify({"response":"Not Found", "code": 404}), 404
     user.pop("password")
     return jsonify(user), 200
+
+@app.route("/entry", methods=["POST"])
+def register_entry():
+    try:
+        data = request.get_json()
+        db.entry.insert_one({
+            "_id": uuid.uuid4().hex,
+            "user_name": data["user_name"],
+            "store_name": data["store_name"],
+            "date": datetime.now().strftime("%d-%b-%Y"),
+            "time": datetime.now().strftime("%H:%M:%S")
+        })
+        return jsonify({"response":"Created", "code": 201}), 201
+    except:
+        return jsonify({"response":"Not Found", "code": 404}), 404
+
+@app.route('/entry', methods=["GET"])
+def get_entry():
+    try:
+        entry = db.entry.find_one({"store_name": request.args.get("store_name")})
+        return jsonify(entry), 200
+    except:
+        return jsonify({"response":"Entry Not Found", "code": 404}), 404
 
 if __name__ == "__main__":
     app.run(port=4032)
